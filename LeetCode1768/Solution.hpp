c49aea2c7e9c2dca37d1c2eb048add311872e8e2
Woo Sung Jahng <jlittle0919@icloud.com>
2024-05-06 02:21:54 +0900

    Initial commit

---
+++ LeetCode1768/Solution.hpp
@@ -0,0 +1,15 @@
+//
+//  Solution.hpp
+//  LeetCode1768
+//
+//  Created by Woo Sung Jahng on 2024/05/06.
+//
+
+#ifndef Solution_hpp
+#define Solution_hpp
+
+#include <stdio.h>
+
+class Solution;
+
+#endif /* Solution_hpp */
